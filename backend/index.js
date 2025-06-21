e8c9c0f627420cfea0ec5a270ad5d7735fa40e40
arnob <farhanasfar21@gmail.com>
2025-06-21 20:50:44 +0600

    Connect to the server: index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDatabase } from "./db/index.js";
+import { app } from "./app.js";
+
+connectDatabase()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    })
+})
+.catch(error => {
+    console.log("Server Error!", error);
+    process.exit(1);
+})
+
