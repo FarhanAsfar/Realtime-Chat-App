ccc66929f79c901d8fd074a56b0b23a57e4c20ac
arnob <farhanasfar21@gmail.com>
2025-06-21 21:26:38 +0600

    Add 'src' folder

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDatabase = async function(){
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB is connected'\n'Host: ${connectionInstance}`);
+        console.log(`MongoDB is connected'\n'Host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("Database connection failed", error);
         process.exit(1);
