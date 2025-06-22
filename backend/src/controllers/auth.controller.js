433f7c4e3a12602ba300be8d79d466e28e0cb543
arnob <farhanasfar21@gmail.com>
2025-06-23 04:58:09 +0600

    Add signout method

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,8 +96,31 @@ const SignIn = asyncHandler(async (req, res) => {
     );
 })
 
+const SignOut = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+
+    await User.findByIdAndUpdate(
+        userId,
+        {
+            $set: {accessToken: undefined}
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .json(
+        new ApiResponse(200, {}, `${req.user.fullName} logged out`)
+    )
+})
+
 
 export{
     SignUp,
     SignIn,
+    SignOut,
 }
