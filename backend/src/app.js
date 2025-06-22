0e285358ff8ac5a9a90a200275bef9ce170031a9
arnob <farhanasfar21@gmail.com>
2025-06-22 07:43:07 +0600

    Add errorHandler middleware to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 
 const app = express();
@@ -9,5 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
+
+
+app.use("/api/v1/auth", authRoutes);
+
+app.use(errorHandler)
 
 export {app}
