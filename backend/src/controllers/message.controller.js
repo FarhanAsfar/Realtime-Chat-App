d20b302ba8f77881f637d90b4012074458ff8610
arnob <farhanasfar21@gmail.com>
2025-06-25 00:27:36 +0600

    Add message controller: getAllUsers Controller

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,25 @@
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Message } from "../models/message.model.js";
+import { User } from "../models/user.model.js";
+
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    const loggedInUserId = req.user._id;
+
+    if(!loggedInUserId){
+        throw new ApiError(404, "Please sign in to your account");
+    }
+
+    //get every users but the loggedInUser for the sidebar
+    const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password"); 
+
+    return res.status(200).json(
+        new ApiResponse(200, {filteredUsers}, "")
+    )
+})
+
+export {
+     getAllUsers,
+}
