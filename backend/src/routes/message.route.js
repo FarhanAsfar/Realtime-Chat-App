2b6ad6b7c633ba3b8b0b34dab7092d7820a11ff7
arnob <farhanasfar21@gmail.com>
2025-06-25 00:37:03 +0600

    Add 'getMessages' route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyAccess } from "../middlewares/auth.middleware.js";
+import { getAllUsers } from "../controllers/message.controller.js";
 
 const messageRouter = Router();
 
 
 messageRouter.route("/users").get(verifyAccess, getAllUsers);
+messageRouter.route("/:id").get(verifyAccess, getMessages);
 
 
 export {
