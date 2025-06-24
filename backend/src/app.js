585c363a4622ecee3b932fefff520080666973a5
arnob <farhanasfar21@gmail.com>
2025-06-25 00:26:10 +0600

    Add message route in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { authRouter } from "./routes/auth.route.js";
 import { userRouter } from "./routes/user.route.js";
+import { messageRouter } from "./routes/message.route.js";
 
 
 const app = express();
@@ -16,6 +17,8 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
+
+app.use("/api/v1/message", messageRouter);
 
 app.use(errorHandler)
 
