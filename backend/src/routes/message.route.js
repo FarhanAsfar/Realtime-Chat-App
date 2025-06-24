2aff5ba273c1f3a21b4f164e33f9042682955c00
arnob <farhanasfar21@gmail.com>
2025-06-25 00:47:42 +0600

    Add sendMessages route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyAccess } from "../middlewares/auth.middleware.js";
-import { getAllUsers } from "../controllers/message.controller.js";
+import { getAllUsers, getMessages, sendMessages } from "../controllers/message.controller.js";
 
 const messageRouter = Router();
 
 
 messageRouter.route("/users").get(verifyAccess, getAllUsers);
 messageRouter.route("/:id").get(verifyAccess, getMessages);
+messageRouter.route("/send/:id").post(verifyAccess, sendMessages);
 
 
 export {
