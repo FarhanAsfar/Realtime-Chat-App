72b0b46781240c0c71b42b2b4d2c7652bf2fdd0a
arnob <farhanasfar21@gmail.com>
2025-06-29 23:47:35 +0600

    Add Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react"
+import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom"
+
 
 const App = () => {
   return (
-    <div>Heeelllooo</div>
+    <div>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </Navbar>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,14 @@ import React, { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
 
-
-console.log("Root Element:", document.getElementById('root'))
+//console.log("Root Element:", document.getElementById('root'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
