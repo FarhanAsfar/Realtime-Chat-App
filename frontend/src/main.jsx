acf0b02abcc02c89c292e22936e1e58f807ff6e0
arnob <farhanasfar21@gmail.com>
2025-06-29 23:35:02 +0600

    Install daisy ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+
+console.log("Root Element:", document.getElementById('root'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
