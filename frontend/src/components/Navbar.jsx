d6b2152cf6128a347b5a3e8b78eff4a07b03185c
arnob <farhanasfar21@gmail.com>
2025-07-02 10:42:36 +0600

    Change 'login' to 'signout'

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import useAuthStore from "../store/useAuthStore";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const {logout, authUser} = useAuthStore(); 
+    const {signout, authUser} = useAuthStore(); 
     
     return (
         <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg ">
@@ -37,7 +37,7 @@ const Navbar = () => {
                                 <span className="hidden sm:inline">Profile</span>
                             </Link>
 
-                            <button className="flex gap-2 items-center" onClick={logout}>
+                            <button className="flex gap-2 items-center" onClick={signout}>
                                 <LogOut className="size-5" />
                                 <span className="hidden sm:inline">Logout</span>
                             </button>
