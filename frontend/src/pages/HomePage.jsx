841532be1b76574528d5e942a3dcd84b0de7e12d
arnob <farhanasfar21@gmail.com>
2025-07-04 12:03:58 +0600

    Import components, fix typo

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import ChatContainer from "../../ChatContainer";
+import NoChatSelected from "../../NoChatSelected";
+import Sidebar from "../components/Sidebar";
 import useChatStore from "../store/useChatStore";
 
 const HomePage = () => {
@@ -6,7 +9,7 @@ const HomePage = () => {
     return (
         <div className="h-screen bg-base-200">
             <div className="flex items-center justify-center pt-20 px-4">
-                <div className="bg-base-100 rounded-lg shadow-cl w-ful max-w-6xl h-[calc(100vh-8rem)">
+                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)">
                     <div callyflex h-full rounded-lg overflow-hidden>
                         <Sidebar />
 
