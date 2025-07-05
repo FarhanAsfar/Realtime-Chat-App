7bdcaa67b9b53ed3b55619a326965b4d58f2ab95
arnob <farhanasfar21@gmail.com>
2025-07-05 11:37:56 +0600

    Add 'online/offline' tag on names

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import useChatStore from "../store/useChatStore";
 import { Users } from "lucide-react";
+import useAuthStore from "../store/useAuthStore";
 
 const Sidebar = () => {
     const {getUsers, users, selectedUser, setSelectedUser, isUserLoading} = useChatStore();
-    
+    const {onlineUsers} = useAuthStore();
+
     useEffect(() => {
         getUsers();
     }, [getUsers])
@@ -44,6 +46,10 @@ const Sidebar = () => {
                         <div className="hidden lg:block text-left min-w-0">
                             <div className="font-medium truncate">
                                 {user.fullName}
+                            </div>
+
+                            <div className="text-sm text-zinc-400">
+                                {onlineUsers.includes(user._id) ? "Online" : "Offline"}
                             </div>
                         </div>
                     </button>
