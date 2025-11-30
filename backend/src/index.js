7aae7c91ff5867c7d754dc247cfa30eab85304c4
arnob <farhanasfar21@gmail.com>
2025-11-30 23:50:09 +0600

    Update import

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { connectDatabase } from "./db/index.js";
-import { server } from "../src/utils/socket.js";
+import { server } from "./utils/socket.js";
 
 connectDatabase()
-.then(() => {
-    server.listen(process.env.PORT, () => {
-        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    .then(() => {
+        server.listen(process.env.PORT, () => {
+            console.log(`Server is running on PORT: ${process.env.PORT}`);
+        })
     })
-})
-.catch(error => {
-    console.log("Server Error!", error);
-    process.exit(1);
-})
+    .catch(error => {
+        console.log("Server Error!", error);
+        process.exit(1);
+    })
 
 
 
